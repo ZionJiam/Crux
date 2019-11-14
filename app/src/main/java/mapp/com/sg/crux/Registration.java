8a705844fe4754f5608fbf844a11495651a032af
Zion Jiam <zionjiam88.18@gmail.com>
2019-11-14 13:36:07 +0800

    All Files

---
+++ app/src/main/java/mapp/com/sg/crux/Registration.java
@@ -0,0 +1,11 @@
+package mapp.com.sg.crux;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Registration extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.registration);
+    }
+}
