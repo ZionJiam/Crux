b4a442f3a29fe65b156b29af7121088767d56753
Zion Jiam <zionjiam88.18@gmail.com>
2019-11-14 22:14:15 +0800

    Added Hello World 2

--- app/src/main/java/mapp/com/sg/crux/Login.java
+++ app/src/main/java/mapp/com/sg/crux/Login.java
@@ -7,5 +7,6 @@ public class Login extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+        //Hello world
     }
 }
